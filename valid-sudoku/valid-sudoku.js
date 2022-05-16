20b27ff46b828904dee89a297343163da3dbe067
LiamNguyen <63218884+Huulamnguyen@users.noreply.github.com>
2022-05-16 16:53:06 -0400

    Time: 89 ms, Memory: 45.1 MB - LeetHub

---
+++ valid-sudoku/valid-sudoku.js
@@ -0,0 +1,47 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    
+    // check the rows
+    for(let i = 0; i < board.length; i++){
+        const set = new Set();
+        for(let j = 0; j < board[i].length; j++){
+            const cell = board[i][j];
+            if(cell === ".") continue;
+            if(set.has(cell)) return false;
+            set.add(cell)
+        }
+    }
+    
+    // check the collumns
+    for(let i = 0; i < board.length; i++){
+        const set = new Set();
+        for(let j = 0; j < board[i].length; j++){
+            const cell = board[j][i];
+            if(cell === ".") continue;
+            if(set.has(cell)) return false;
+            set.add(cell)
+        }
+    }
+    
+    // check the boxes by sub-boxes
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            
+            const set = new Set();
+            for(let k = 0; k < 3; k++){
+                for(let l = 0; l < 3; l++){
+                    const cell = board[3*i+k][3*j+l]
+                    if(cell === ".") continue;
+                    if(set.has(cell)) return false;
+                    set.add(cell)
+                }
+            }
+        }
+    }
+    
+    return true;
+    
+};
