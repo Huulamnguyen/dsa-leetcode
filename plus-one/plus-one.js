4efd45ff0206ee31c09745c60a5e9f8c5f3ddd9c
LiamNguyen <63218884+Huulamnguyen@users.noreply.github.com>
2022-05-16 12:02:33 -0400

    Time: 104 ms, Memory: 42.3 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -4,7 +4,11 @@
  */
 var plusOne = function(digits) {
     const n = digits.length;
+    
+    //itergrate over the array start from the end
     for ( let i = n - 1; i >= 0; i--){
+        
+        
         if(digits[i] == 9){
             digits[i] = 0;
         } else {
