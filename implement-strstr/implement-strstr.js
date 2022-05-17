19f63eceb5e3a50ab8f50203c71a2786e105f0b7
LiamNguyen <63218884+Huulamnguyen@users.noreply.github.com>
2022-05-17 16:17:48 -0400

    Time: 73 ms, Memory: 42 MB - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if(needle==="") return 0;
+  
+  if(haystack.includes(needle)){
+    return haystack.indexOf(needle)
+  }
+  
+  return -1
+};
