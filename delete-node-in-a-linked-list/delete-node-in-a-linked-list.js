af13b0c56941432fda66fb7a256126959dd4cb0f
LiamNguyen <liamnguyen.swe@gmail.com>
2022-07-11 21:11:49 -0400

    Time: 121 ms, Memory: 43.9 MB - LeetHub

--- delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
@@ -10,13 +10,12 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-    // Singly linked list
-    // the node to be deleted
-    
+  
   if(node === null || node.next === null){
     return null
   }
   
   node.val = node.next.val;
-  node.next = node.next.next
+  node.next = node.next.next;
+
 };
