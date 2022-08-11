1a08591bb87b5b9887d28398c85189d7c7f32206
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-11 09:22:03 -0400

    Time: 130 ms (16.63%), Space: 44 MB (54.36%) - LeetHub

--- 139-word-break/139-word-break.js
+++ 139-word-break/139-word-break.js
@@ -3,22 +3,19 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
-    
-  const table = new Array(s.length + 1).fill(false)
-    
-  table[0] = true
+var wordBreak = function(s, wordDict, memo={}) {
+    if (s in memo) return memo[s]
+  if(s === '') return true
   
-  for(let i=0; i<table.length; i++){
-    if(table[i] === false) continue;
-    for(let j=i+1; j<table.length; j++){
-      if(wordDict.includes(s.slice(i,j))){
-        table[j] = true
+  for(let word of wordDict){
+    if(s.indexOf(word) === 0){
+      let suffix = s.slice(word.length);
+      if(wordBreak(suffix,wordDict,memo) === true){
+        memo[s] = true
+        return memo[s]
       }
     }
   }
-  
-  return table[table.length - 1];
-  
-  
+  memo[s] = false
+  return memo[s]
 };
