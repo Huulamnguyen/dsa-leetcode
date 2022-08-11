6ac2e3e24352ac9f6fb45ed0d2e39125943cd04c
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-10 20:31:26 -0400

    Time: 114 ms (33.29%), Space: 44.4 MB (29.36%) - LeetHub

--- 139-word-break/139-word-break.js
+++ 139-word-break/139-word-break.js
@@ -5,17 +5,17 @@
  */
 var wordBreak = function(s, wordDict) {
   
-  const obj = new Set(wordDict)
+  const set = new Set(wordDict)
   const ht = {}
   
   function inTheDict(string){
-    if( string == "" || obj.has(string) ) return true
+    if( string == "" || set.has(string) ) return true
     if( ht[string] !== undefined ) return ht[string]
     
     let temp = ""
     for(let i = 0; i < string.length; i++){
       temp += string[i]
-      if(obj.has(temp)){
+      if(set.has(temp)){
         if(inTheDict(string.slice(i+1))){
           ht[string] = true
           return true
