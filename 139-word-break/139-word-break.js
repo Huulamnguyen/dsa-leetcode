16375fabac26d207662a51bf9f15e0609dbeb365
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-10 22:36:31 -0400

    Time: 77 ms (86.02%), Space: 43.6 MB (73.91%) - LeetHub

--- 139-word-break/139-word-break.js
+++ 139-word-break/139-word-break.js
@@ -5,29 +5,42 @@
  */
 var wordBreak = function(s, wordDict) {
   
-  const set = new Set(wordDict)
-  const ht = {}
+//   const set = new Set(wordDict)
+//   const ht = {}
   
-  function inTheDict(string){
-    if( string == "" || set.has(string) ) return true
-    if( ht[string] !== undefined ) return ht[string]
+//   function inTheDict(string){
+//     if( string == "" || set.has(string) ) return true
+//     if( ht[string] !== undefined ) return ht[string]
     
-    let temp = ""
-    for(let i = 0; i < string.length; i++){
-      temp += string[i]
-      if(set.has(temp)){
-        if(inTheDict(string.slice(i+1))){
-          ht[string] = true
-          return true
-        }
+//     let temp = ""
+//     for(let i = 0; i < string.length; i++){
+//       temp += string[i]
+//       if(set.has(temp)){
+//         if(inTheDict(string.slice(i+1))){
+//           ht[string] = true
+//           return true
+//         }
+//       }
+//     }
+    
+//     ht[string] = false
+//     return false
+    
+//   }
+  
+//   return inTheDict(s)
+  
+  const table = new Array(s.length + 1).fill(false)
+  
+  table[0] = true
+  for (let i=0; i<table.length; i++){
+    if(table[i] === false) continue;
+    for(let j=i+1; j<table.length; j++){
+      if(wordDict.includes(s.slice(i,j))){
+        table[j] = true
       }
     }
-    
-    ht[string] = false
-    return false
-    
   }
   
-  return inTheDict(s)
-  
+  return table[table.length - 1]
 };
