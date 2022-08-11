d282f047df9ce853a7bb41a09c26abe42e8c2bc4
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-11 09:29:59 -0400

    Time: 113 ms (24.81%), Space: 42.9 MB (22.19%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,20 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  
-  const brackets = {
-    '(':')',
-    '{':'}',
-    '[':']'
+    
+  const brackets ={
+   '(':')',
+   '{':'}', 
+   '[':']'   
   }
   
-  const stack = [];
+  const stack = []
   
-  for(let char of s){
+  for (char of s){
     if(brackets[char]) stack.push(char)
     else if(stack.length > 0 && brackets[stack[stack.length - 1]] === char) stack.pop()
     else return false
   }
   
   return stack.length === 0
+  
 };
