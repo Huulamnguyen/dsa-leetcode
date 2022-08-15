e12bb070aef5ff24d763a7f4b7d6078b930187e6
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-15 12:56:59 -0400

    Time: 102 ms (33.50%), Space: 14.1 MB (90.94%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+        location = [0,0]
+        
+        for step in moves:
+          match step:
+            case "L":
+              location[0] -= 1
+            case "R":
+              location[0] += 1
+            case "U":
+              location[1] += 1
+            case default:
+              location[1] -= 1
+        
+        return True if location[0] == 0 and location[1] == 0 else False
