722ab5ae22929bdf1d8eac4f0f3d91916b7e5b25
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-22 10:04:30 -0400

    Time: 104 ms (77.22%), Space: 46.3 MB (83.52%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+var subdomainVisits = function(cpdomains) {
+    
+    const domainCount = {};
+    
+    for (let i = 0; i < cpdomains.length; i++){
+        
+        let count = cpdomains[i].split(" ")[0]
+        let domain = cpdomains[i].split(" ")[1]
+        
+        // console.log(count)
+        // console.log(domain)
+        
+        while(1){
+            if(!domainCount[domain]){
+                domainCount[domain] = Number(count)
+            } else {
+                domainCount[domain] += Number(count)
+            }
+            
+             if(!domain.includes(".")) {
+                break
+            }
+            
+            domain = domain.substr(domain.indexOf(".") + 1)
+            // console.log(domain)
+        }
+    }
+    
+    const res = []
+    
+    for (let ele in domainCount){
+        res.push(domainCount[ele] + " " + ele)
+    }
+    return res
+};
