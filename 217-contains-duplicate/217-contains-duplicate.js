96ed9a6f788a17d53d59ca91178a6249238ef59d
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-23 16:38:38 -0400

    Time: 252 ms (20.69%), Space: 54.6 MB (5.49%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,11 +3,25 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b) => b - a)
-    for(let i=0; i < nums.length; i++){
-        if(i > 0 && nums[i] === nums[i-1]) return true
+//     nums.sort((a,b) => b - a)
+//     for(let i=0; i < nums.length; i++){
+//         if(i > 0 && nums[i] === nums[i-1]) return true
+//     }
+    
+//     return false
+    
+    const ht = {}
+    let res = false
+    
+    for (let num of nums){
+        if (!ht[num]) ht[num] = 1
+        else ht[num]++
     }
     
-    return false
+    for (let key in ht){
+        if (ht[key] > 1) return res = true
+    }
+    
+    return res
     
 };
