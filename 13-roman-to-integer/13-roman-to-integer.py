2f61ad5b3419333fc4377deb135a1eb716f28311
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-23 11:18:54 -0400

    Time: 63 ms (72.93%), Space: 13.9 MB (31.05%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,33 +1,22 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-    const roman = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    
-    let res = 0;
-    
-    for(let i=0; i<s.length; i++){
-        let current = roman[s[i]]
-        let next = roman[s[i+1]]
+class Solution:
+    def romanToInt(self, s: str) -> int:
         
-        if(current < next){
-            res += (next - current)
-            i++
-        } else {
-            res += current
+        rd = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
-    }
-    
-    return res
-    
-};
+        
+        n = len(s)
+        rt = 0
+        for i in range(n):
+            if  i==n-1 or rd[s[i]] >= rd[s[i+1]] :
+                rt += rd[s[i]]
+            else :
+                rt -= rd[s[i]]
+                
+        return rt
