d339e009c3c87e93c6ec47c6b2f33c0a7f434d21
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-24 11:01:54 -0400

    Time: 71 ms (31.43%), Space: 13.8 MB (99.75%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,24 +1,14 @@
-/**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
- */
-var wordBreak = function(s, wordDict) {
-    
-    // Create a table array that filled with all 'false' with the length of s
-    const table = new Array(s.length + 1).fill(false)
-    
-    table[0] = true
-    
-    for (let i=0; i < table.length; i++){
-        if(table[i] === false) continue
-        for(let j = i+1; j < table.length; j++){
-            if(wordDict.includes(s.slice(i,j))){
-                table[j] = true
-            }
-        }
-    }
-    
-    return table[table.length -1]
-    
-};
+class Solution:
+	def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+		#1d dp from last to the first,neetcode
+		l=len(s)
+		dp=[False]*(l+1) 
+		dp[l]=True
+		for i in range(l-1,-1,-1):
+			for w in wordDict:
+				if (i+len(w))<=l and s[i:i+len(w)]==w:
+					dp[i]=dp[i+len(w)]
+				if dp[i]:
+					break
+
+		return dp[0]
