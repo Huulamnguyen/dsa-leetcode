c5c0a3c18a19142f4939f3f56c1b06a43650ef26
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-25 11:09:40 -0400

    Time: 95 ms (47.43%), Space: 14.1 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -2,7 +2,6 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         ransomNoteDict = Counter(ransomNote)
         magazineDict = Counter(magazine)
-        
         for k in ransomNoteDict:
             magazineDict[k] = magazineDict.get(k, 0) - ransomNoteDict[k]
             if magazineDict[k] < 0:
