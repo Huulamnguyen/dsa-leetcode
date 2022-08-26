34d57d2d076c0fdc91b7fe85c711e328dc3d828c
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-26 10:22:29 -0400

    Time: 98 ms (34.29%), Space: 44 MB (62.86%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.js
+++ 869-reordered-power-of-2/869-reordered-power-of-2.js
@@ -4,24 +4,20 @@
  */
 var reorderedPowerOf2 = function(n) {
     
-    // If the value is 1 return true
-    if(n===1) return true;
+    if ( n === 1 ) return true
     
     let total = 1;
     let i = 1;
     
-	// coverting the given number and sorted that with any order(ASC/DESC) 
-    let nTemp = n.toString().split('').sort((a,b)=>a-b).join('');
+    let nTemp = n.toString().split('').sort((a,b) => a-b).join('')
     
-	// If the length of the string is more than the given one means we can break the loop and return false 
-    while(total.toString().length <= n.toString().length){
+    while (total.toString().length <= n.toString().length){
         
-        total = Math.pow(2,i);
-        let tTemp = total.toString().split('').sort((a,b)=>a-b).join('');
-		
-        // Comparing the pow of 2 and the given number
-        if(tTemp===nTemp) return true;
-        i++;
+        total = Math.pow(2, i)
+        let tTemp = total.toString().split('').sort((a,b) => a-b).join('')
+        
+        if (nTemp === tTemp) return true
+        i++
     }
-    return false;
+    return false
 };
