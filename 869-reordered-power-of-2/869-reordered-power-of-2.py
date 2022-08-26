1ed471bf689780fecdab717f8e81344673ebee35
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-26 09:54:53 -0400

    Time: 65 ms (45.99%), Space: 13.9 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -2,7 +2,6 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         
         digits = Counter(str(n))
-        print(digits)
         
         for i in range(30):
             if digits == Counter(str(1 << i)):
