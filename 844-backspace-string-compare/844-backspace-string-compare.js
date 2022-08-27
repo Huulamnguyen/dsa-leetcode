551b8fed0a217db8c57996cd76e67c4daad06580
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-27 14:14:01 -0400

    Time: 84 ms (68.72%), Space: 43.1 MB (34.95%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    
+    s = Array.from(s);
+    t = Array.from(t);
+    
+    let i = 0;
+    
+    while (s[i]) {
+        
+        if (s[0] === "#") s.shift(); // Shift if # at the begininng.
+        else if (s[i] === "#") {s.splice(i-1, 2); i--;} // Remove the letter and the '#'
+        else i++;
+    }
+    
+    i = 0;
+    
+    while (t[i]) {
+        
+        if (t[0] === "#") t.shift();
+        else if (t[i] === "#") {t.splice(i-1, 2); i--;}
+        else i++;
+    }
+    
+    if (s.join("") === t.join("")) return true; 
+    else return false;
+};
