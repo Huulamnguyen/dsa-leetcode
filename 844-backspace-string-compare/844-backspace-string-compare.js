43f9b0312639e84ded36337a40d626eb5bcd4b82
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-27 14:22:03 -0400

    Time: 69 ms (90.32%), Space: 42.8 MB (52.16%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -5,32 +5,37 @@
  */
 var backspaceCompare = function(s, t) {
     
-    s = Array.from(s);
-    t = Array.from(t);
+    s = Array.from(s)
+    t = Array.from(t)
     
-    let i = 0;
+    let i = 0
     
-    while (s[i]) {
-        
-        if (s[0] === "#") s.shift(); // Shift if # at the begininng.
-        else if (s[i] === "#") {
-            s.splice(i-1, 2); 
+    while(s[i]){
+        if (s[0] === "#") s.shift()
+        else if (s[i] === "#"){
+            s.splice(i-1,2);
             i--;
-        } // Remove the letter and the '#'
-        else i++;
+        }else{
+            i++;
+        }
     }
     
     i = 0;
     
-    while (t[i]) {
-        
-        if (t[0] === "#") t.shift();
-        else if (t[i] === "#") {
-            t.splice(i-1, 2); 
+    while(t[i]){
+        if(t[0] === "#") t.shift()
+        else if (t[i] === "#"){
+            t.splice(i-1,2);
             i--;
+        }else{
+            i++;
         }
-        else i++;
     }
     
-    return true ? s.join("") === t.join("") : false
+    // console.log(s.join(''))
+    // console.log(t.join(''))
+    
+    
+    return true ? s.join('') === t.join('') : false
+    
 };
