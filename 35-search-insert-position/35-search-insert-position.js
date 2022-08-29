6f2f1a6830d35c540b3cc38d1f76cc823d558dce
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-29 10:15:38 -0400

    Time: 101 ms (27.00%), Space: 42.1 MB (74.22%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -5,12 +5,18 @@
  */
 var searchInsert = function(nums, target) {
 
-    if (nums.includes(target)){
-        return nums.indexOf(target)
-    } else {
-        nums.push(target)
-        nums.sort((a,b) => a-b)
-        return nums.indexOf(target)
+    // if (nums.includes(target)){
+    //     return nums.indexOf(target)
+    // } else {
+    //     nums.push(target)
+    //     nums.sort((a,b) => a-b)
+    //     return nums.indexOf(target)
+    // }
+    
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] === target || target < nums[i]) return i
     }
+    
+    return nums.length
     
 };
