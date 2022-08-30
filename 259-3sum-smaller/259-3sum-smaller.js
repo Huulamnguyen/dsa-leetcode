9f57afb2bdfac2724992a23e1e7b93b2e1ce03ea
LiamNguyen <liamnguyen.swe@gmail.com>
2022-08-30 14:45:44 -0400

    Time: 121 ms (60.12%), Space: 44.5 MB (19.02%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.js
+++ 259-3sum-smaller/259-3sum-smaller.js
@@ -5,26 +5,27 @@
  */
 var threeSumSmaller = function(nums, target) {
     
-    nums.sort((a,b) => a - b)
+    nums.sort((a,b) => a-b)
     
     function twoSumSmaller(startIdx, target){
-        let twoSumCount = 0
-        let left = startIdx
+        let twoSumCount = 0;
+        let left = startIdx;
         let right = nums.length - 1
+        
         while(left < right){
             if(nums[right] + nums[left] < target){
                 twoSumCount += right - left
-                left++
-            } else {
+                left++;
+            }else{
                 right--
             }
         }
-        return twoSumCount
+        return twoSumCount;
     }
     
-    let threeSumCount = 0
+    let threeSumCount = 0;
     for(let i = 0; i < nums.length - 2; i++){
         threeSumCount += twoSumSmaller(i+1, target - nums[i])
     }
-    return threeSumCount
+    return threeSumCount;
 };
