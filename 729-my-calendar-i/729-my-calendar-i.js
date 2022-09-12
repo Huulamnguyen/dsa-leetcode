21bff83c9af9eba25c460491e91e5380ebd411c9
BoBaGuy <liamnguyen.swe@gmail.com>
2022-09-12 12:36:30 -0400

    Time: 366 ms (20.29%), Space: 49.7 MB (99.36%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.js
@@ -0,0 +1,24 @@
+
+var MyCalendar = function() {
+    
+    cal = []
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(start, end) {
+    for(let q=0;q<cal.length;q++){
+        if(start<cal[q][1] && end>cal[q][0])return false
+    }
+    cal.push([start,end])
+    return true
+};
+
+/** 
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
